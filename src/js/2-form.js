9af9c810d0cbd75a76896c184eb61444e7ea3aa8
Daryna <buriak.daryna@gmail.com>
2025-01-06 21:09:53 +0200

    Add task 2

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const feedbackForm = document.querySelector('.feedback-form');
+feedbackForm.addEventListener('submit', handleSubmit);
+feedbackForm.addEventListener('input', handleInput);
+
+function handleInput(event) {
+  const inputSrc = event.target;
+  const inputName = inputSrc.name;
+  const inputValue = inputSrc.value;
+
+  formData[inputName] = inputValue;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+
+  if (email === '' || message === '') {
+    return alert('Please fill all fields');
+  }
+
+  console.log(formData);
+
+  form.reset();
+  localStorage.removeItem('feedback-form-state');
+  formData.email = '';
+  formData.message = '';
+}
+
+const savedItem = JSON.parse(localStorage.getItem('feedback-form-state'));
+if (savedItem !== null) {
+  feedbackForm.elements.email.value = savedItem.email;
+  feedbackForm.elements.message.value = savedItem.message;
+}
